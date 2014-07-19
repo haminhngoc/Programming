ed7cca5c99f9f8b14cd4bfd4249c042a9644241c
Ngoc <haminhngoc@yahoo.com>
2014-07-19 16:11:46 +0700

    .............

--- Ngoc/BaseCode/BaseCode.java
+++ Ngoc/BaseCode/BaseCode.java
@@ -9,11 +9,21 @@ public class BaseCode {
 	public static void main(String[] args) throws IOException {
 		initReader();
 		while (true) {
-			readInput();
+			// readInput();
 
-			int result = solve();
+			// int result = solve();
 
-			System.out.println(result);
+			// System.out.println(result);
+
+			print(1, 2, 3, 4);
+			println(1, 2, 3, 4);
+			int[] a = new int[10];
+			for (int i = 0; i < a.length; i++) {
+				a[i] = i;
+			}
+			println(join(a));
+			Object[] b = new Object[5];
+			println(b);
 
 			break;
 		}
@@ -57,5 +67,110 @@ public class BaseCode {
 
 	static Double nextDouble() throws IOException {
 		return Double.parseDouble(next());
+	}
+
+	public static int[] readLine(int n) throws IOException {
+		int[] result = new int[n];
+		for (int i = 0; i < n; i++) {
+			result[i] = nextInt();
+		}
+		return result;
+	}
+
+	public static long[] readLineLong(int n) throws IOException {
+		long[] result = new long[n];
+		for (int i = 0; i < n; i++) {
+			result[i] = nextLong();
+		}
+		return result;
+	}
+
+	public static int[][] readMatrix(int row, int column) throws IOException {
+		int[][] result = new int[row][column];
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				result[i][j] = nextInt();
+			}
+		}
+		return result;
+	}
+
+	public static long[][] readMatrixLong(int row, int column)
+			throws IOException {
+		long[][] result = new long[row][column];
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < column; j++) {
+				result[i][j] = nextLong();
+			}
+		}
+		return result;
+	}
+
+	public static void print(Object... obj) {
+		System.out.print(join(obj, " "));
+	}
+
+	public static void println(Object... obj) {
+		System.out.println(join(obj, " "));
+	}
+
+	public static String join(Object[] objs) {
+		return join(objs, " ");
+	}
+
+	public static String join(int[] objs) {
+		return join(objs, " ");
+	}
+
+	public static String join(long[] objs) {
+		return join(objs, " ");
+	}
+
+	public static String join(Object[] objs, String glue) {
+		if (objs.length == 0) {
+			return "";
+		}
+		StringBuilder sb = new StringBuilder();
+		int i;
+		for (i = 0; i < objs.length - 1; i++) {
+			sb.append(objs[i] + glue);
+		}
+		return sb.toString() + objs[i];
+	}
+
+	public static String join(int[] objs, String glue) {
+		if (objs.length == 0) {
+			return "";
+		}
+		StringBuilder sb = new StringBuilder();
+		int i;
+		for (i = 0; i < objs.length - 1; i++) {
+			sb.append(objs[i] + glue);
+		}
+		return sb.toString() + objs[i];
+	}
+
+	public static String join(long[] objs, String glue) {
+		if (objs.length == 0) {
+			return "";
+		}
+		StringBuilder sb = new StringBuilder();
+		int i;
+		for (i = 0; i < objs.length - 1; i++) {
+			sb.append(objs[i] + glue);
+		}
+		return sb.toString() + objs[i];
+	}
+
+	public static void printMatrix(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			println(join(matrix[i]));
+		}
+	}
+
+	public static void printMatrix(long[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			println(join(matrix[i]));
+		}
 	}
 }
