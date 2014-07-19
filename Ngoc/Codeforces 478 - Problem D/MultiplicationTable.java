54a7dbd5b67cddd6b751ede331202ef7b8ed6bdf
Ngoc <haminhngoc@yahoo.com>
2014-07-19 10:32:21 +0700

    ,,,,,,,,,,

--- Ngoc/Codeforces 478 - Problem D/MultiplicationTable.java
+++ Ngoc/Codeforces 478 - Problem D/MultiplicationTable.java
@@ -8,15 +8,8 @@ public class MultiplicationTable {
 	static int m; // 500K
 	static int k; // n * m
 
-	static int upperStart;
-	static int upperEnd;
-	static int[] upperBoundIndex;
-	static int upperCount;
-
-	static int lowerStart;
-	static int lowerEnd;
-	static int[] lowerBoundIndex;
-	static int[] lowerCount;
+	static int[] lowPartition;
+	static int[] upPartition;
 
 	public static void main(String[] args) throws IOException {
 		initReader();
@@ -38,14 +31,86 @@ public class MultiplicationTable {
 			m = n;
 			n = temp;
 		}
-		upperBoundIndex = new int[m];
-		upperBoundIndex = new int[m];
+		k = m * n - k;
+		// int lowPartitionCount = 1;
+		lowPartition = new int[m + 2];
+		lowPartition[0] = 1;
+
+		// int upPartitionCount = 1;
+		upPartition = new int[m + 2];
+		for (int i = 0; i < m; i++) {
+			upPartition[i] = 0;
+		}
+		upPartition[m] = n;
+		upPartition[m + 1] = m + 1;
+
+		int[] partition = new int[m + 2];
 
 		while (true) {
+
+			int validCount = 0;
+			for (int i = 1; i <= m; i++) {
+				if (upPartition[i] > lowPartition[i] + 1) {
+					validCount++;
+					partition[i] = (upPartition[i] - lowPartition[i]) / 2;
+				}
+			}
+			if (validCount > 0) {
+				validCount = (validCount + 1) / 2;
+				int columnPivot = 1;
+				for (int i = 1; i <= m; i++) {
+					if (upPartition[i] > lowPartition[i] + 1)
+						validCount--;
+					if (validCount == 0) {
+						columnPivot = i;
+						break;
+					}
+				}
+
+				int partitionCount = makePartition(partition, columnPivot);
+				if (k < partitionCount) {
+					upPartition = partition;
+				} else {
+					lowPartition = partition;
+				}
+			} else {
+				List<Integer> inPartition = new ArrayList<Integer>();
+				//LATER				
+			}
+
 			break;
 		}
 
 		return 0;
+	}
+
+	public static int makePartition(int[] partition, int columnPivot) {
+
+		int maxItem = columnPivot * partition[columnPivot];
+		int partitionCount = columnPivot * partition[columnPivot];
+		for (int i = columnPivot + 1; i < m; i++) {
+			int j = partition[columnPivot];
+			for (; j >= 1; j--) {
+				if (i * j < maxItem) {
+					break;
+				}
+			}
+			partition[i] = j;
+			partitionCount += j;
+		}
+
+		for (int i = columnPivot; i > 1; i--) {
+			int j = partition[columnPivot];
+			for (; j < n; j++) {
+				if (i * j < maxItem) {
+					break;
+				}
+			}
+			partition[i] = j;
+			partitionCount += i;
+		}
+
+		return partitionCount;
 	}
 
 	public static void readInput() throws IOException {
