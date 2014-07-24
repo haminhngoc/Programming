b1fb05f0a2c7039235753b519d23772e2d1925b8
Ngoc <haminhngoc@yahoo.com>
2014-07-24 22:20:27 +0700

    ........

--- Ngoc/CodeForces 427 - Problem C/Checkposts.java
+++ Ngoc/CodeForces 427 - Problem C/Checkposts.java
@@ -3,13 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class BaseCode {
-	static boolean test = false;
+public class Checkposts {
+	static boolean test = true;
 
 	static int n; // 10^6
 
 	public static void main(String[] args) throws IOException {
-		
 		initReader();
 		logTime("");
 		while (true) {
--- Ngoc/CodeForces 450 - Problem E/JzzhuAndApples.java
+++ Ngoc/CodeForces 450 - Problem E/JzzhuAndApples.java
@@ -3,13 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class BaseCode {
-	static boolean test = false;
+public class JzzhuAndApples {
+	static boolean test = true;
 
 	static int n; // 10^6
 
 	public static void main(String[] args) throws IOException {
-		
 		initReader();
 		logTime("");
 		while (true) {
