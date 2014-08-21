8ce56a3e2601278053d939e62329eb4ecd72edf2
khanh <khanh.nguyen.k1set@eiu.edu.vn>
2014-08-21 08:23:26 +0700

    update a5 a6

---
+++ Khanh/Array/A8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class A8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = scan.nextInt();
+		
+		
+		
+		scan.close();
+	}
+
+}
