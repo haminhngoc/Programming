5467b6ba3b0c92f66f0efedd89deeffd4c4fbb0d
khanh <khanh.nguyen.k1set@eiu.edu.vn>
2014-10-04 11:30:48 +0700

    4/10

---
+++ Olympic-2014/Khanh/12770. Bắn máy bay/C11TRCNT_12770.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class C11TRCNT_12770 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int x[]=new int[n];
+		int y[]=new int[n];
+		for(int i=0; i<n; i++){
+			x[i]=scan.nextInt();
+			y[i]=scan.nextInt();
+		}
+		scan.close();
+	}
+}
