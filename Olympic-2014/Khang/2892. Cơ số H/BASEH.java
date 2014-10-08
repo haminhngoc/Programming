1d0dfb2b5af92272a0e019cb0eb6169096279a93
bobobo593 <khang.nguyen.k1set@eiu.edu.vn>
2014-10-08 12:59:51 +0700

    a

--- Olympic-2014/Khang/2892. Cơ số H/BASEH.java
+++ Olympic-2014/Khang/2892. Cơ số H/BASEH.java
@@ -16,16 +16,25 @@ class BASEHSolver {
 	public void solve() {
 		int k = in.nextInt();
 		int h = in.nextInt();
-		int a[] = new int[10];
-		a[0] = 0;
+		int a[] = new int[30];		
 		a[1] = 1;
-		for (int i = 2; i < 10; i++) {			
-			if (i % 2 == 0) {
+		for (int i = 2; i < 30; i++) {			
+			if (i % 2 == 0) {				
 				a[i] = h * a[i/2];				
 			} else {
 				a[i] = h * a[(i - 1)/2] + 1;
 			}
-			System.out.println(i + ": "+ a[i]);
+			System.out.println(i + ": "+ a[i] + " ---> " + ConverToBaseH(a[i], h));
 		}
+	}
+	
+	private static int ConverToBaseH(int number, int H) {
+		StringBuilder tmp = new StringBuilder();
+		while (number >= 1) {
+			tmp.append(number % H);
+			number /= H;
+			
+		}
+		return Integer.parseInt(tmp.toString());
 	}
 }
