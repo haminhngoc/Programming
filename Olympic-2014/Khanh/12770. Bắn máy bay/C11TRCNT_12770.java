46d0a3b6be22cbf69ce0d32ac4aa1bc48459c305
khanh <khanh.nguyen.k1set@eiu.edu.vn>
2014-10-08 10:05:34 +0700

    8/10

--- Olympic-2014/Khanh/12770. Bắn máy bay/C11TRCNT_12770.java
+++ Olympic-2014/Khanh/12770. Bắn máy bay/C11TRCNT_12770.java
@@ -1,4 +1,8 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Point12770 {
 	int x = 0;
@@ -11,25 +15,21 @@ class Point12770 {
 		x = _x;
 		y = _y;
 	}
-
-	double getRange(Point12770 b) {
-		double result = 0;
-		result = Math.sqrt(Math.pow(x - b.x, 2) + Math.pow(y - b.y, 2));
-		return result;
-	}
 }
 
 class C11TRCNT_12770 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
+		InputStream inputStream = System.in;
+		InputReader12770 in = new InputReader12770(inputStream);
+
+		int n = in.nextInt();
 		Point12770 a[] = new Point12770[n];
 		for (int i = 0; i < n; i++)
-			a[i] = new Point12770(scan.nextInt(), scan.nextInt());
+			a[i] = new Point12770(in.nextInt(), in.nextInt());
 
-		int min = 201;
+		int min = Integer.MAX_VALUE;
 		int iMin = 0;
 		int size = 0;
 		int count[] = new int[n];
@@ -39,10 +39,8 @@ class C11TRCNT_12770 {
 		for (int i = 0; i < n - 2; i++) {
 			for (int j = i + 1; j < n - 1; j++)
 				for (int k = j + 1; k < n; k++) {
-					double sA = a[i].getRange(a[j]);
-					double sB = a[j].getRange(a[k]);
-					double sC = a[k].getRange(a[i]);
-					if (sA < sB + sC && sB < sA + sC && sC < sA + sB) {
+					if (a[i].x * (a[j].y - a[k].y) + a[j].x * (a[k].y - a[i].y)
+							+ a[k].x * (a[i].y - a[j].y) != 0) {
 						size++;
 						count[i]++;
 						count[j]++;
@@ -55,6 +53,31 @@ class C11TRCNT_12770 {
 			}
 		}
 		System.out.print(size + " " + iMin);
-		scan.close();
 	}
+}
+
+class InputReader12770 {
+	public BufferedReader reader;
+	public StringTokenizer tokenizer;
+
+	public InputReader12770(InputStream stream) {
+		reader = new BufferedReader(new InputStreamReader(stream), 32768);
+		tokenizer = null;
+	}
+
+	public String next() {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			try {
+				tokenizer = new StringTokenizer(reader.readLine());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return tokenizer.nextToken();
+	}
+
+	public int nextInt() {
+		return Integer.parseInt(next());
+	}
+
 }
