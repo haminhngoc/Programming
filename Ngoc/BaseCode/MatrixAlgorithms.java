e64a5ed45f255eee3335f7a2b943589b26c269e0
haminhngoc <haminhngoc@yahoo.com>
2014-10-25 07:39:18 +0700

    LCS - QueryAll; MAXTRIX: minPath & MaxSquare

--- Ngoc/BaseCode/MatrixAlgorithms.java
+++ Ngoc/BaseCode/MatrixAlgorithms.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 
-public class TableAlgorithms {
+public class MatrixAlgorithms {
 
-	// Input: matrix(m, n)
+	// Input: boolean matrix(m, n)
 	// true: Can jump in
 	// false: Can not jump in
 	static int[][] findMinPath(boolean[][] matrix) {
@@ -80,9 +80,65 @@ public class TableAlgorithms {
 		return tracker;
 	}
 
-	// Tim hinh vuong lon nhat
+	// Input: boolean matrix(m, n)
+	// => Find max Square contains only false
+	public static Rectangle findMaxSquare(boolean[][] matrix) {
+		int row = matrix.length;
+		int column = matrix[0].length;
 
-	// Tim hinh chu nhat lon nhat
+		int[] square = new int[column]; // MaxSquare from each position to LeftTop
+		int[] vertical = new int[column]; // MaxHeigh from each position
+
+		Rectangle result = new Rectangle(-1, -1, 0, 0);
+		for (int i = 0; i < row; i++) {
+			int segment = 0;
+			int pre = 0;
+			for (int j = 0; j < column; j++) {
+				if (!matrix[i][j]) {
+					segment++;
+					vertical[j]++;
+				}
+				else {
+					segment = 0;
+					vertical[j] = 0;
+				}
+				int current = square[j];
+				square[j] = Math.min(pre + 1, Math.min(segment, vertical[j]));
+				pre = current;
+
+				if (square[j] > result.width) {
+					result.width = result.height = square[j];
+					result.x = i - result.width + 1;
+					result.y = j - result.height + 1;
+				}
+			}
+		}
+		return result;
+	}
+
+	static class Rectangle {
+		int x;
+		int y;
+		int width;
+		int height;
+
+		public Rectangle(int x, int y, int width, int height) {
+			this.x = x;
+			this.y = y;
+			this.width = width;
+			this.height = height;
+		}
+
+		public String toString() {
+			return "(" + x + "," + y + ":" + width + "," + height + ")";
+		}
+	}
+
+	// Input: boolean matrix(m, n)
+	// => Find max Rectangle contains only false
+	public static Rectangle findMaxRectangle(boolean[][] matrix) {
+		return null;
+	}
 
 	public static void main(String[] args) {
 		boolean[][][] matrixTestcases = new boolean[][][] {
@@ -93,13 +149,23 @@ public class TableAlgorithms {
 				{ true, true, false, true, true, false, true, false, false },
 				{ true, true, false, true, true, false, true, true, true },
 				{ true, true, true, true, true, false, true, true, true }
-		}
+		},
+				{
+				{ true, false, false, true, true, true, true, true, true },
+				{ true, false, false, false, false, false, false, false, true },
+				{ true, false, false, false, false, false, false, false, true },
+				{ true, false, false, true, false, false, false, false, true },
+				{ true, false, true, true, false, false, false, false, true },
+				{ true, true, true, true, true, true, false, false, true }
+		},
 		};
 
 		for (int i = 0; i < matrixTestcases.length; i++) {
 			boolean[][] matrix = matrixTestcases[i];
 			int[][] path = findMinPath(matrix);
 			printMatrix(path);
+			Rectangle maxRec = findMaxSquare(matrix);
+			System.out.println(maxRec.toString());
 		}
 	}
 
